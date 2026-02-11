0dc61de2361f70f99feeb138a744c61bb63c7c7b
Timoteo Costantini Scala <timoteocostantiniscala@gmail.com>
2026-02-11 17:07:36 +0100

    Add files via upload

---
+++ ft_custom_errors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class GardenError:
+    pass
+
+class PlantError(GardenError): #Errors with plants
+    pass
+
+class WaterError(GardenError): #Errors with watering
+    pass
+
+
